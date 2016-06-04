ea930dd0d8dabb35774ab7360f3ee345d7d10a67
Brandon Wallace <branwall@me.com>
2016-06-04 14:07:28 -0700

    Got some buttons working! Adding logic and assets.

--- inTheOwl/GameScene.swift
+++ inTheOwl/GameScene.swift
@@ -9,15 +9,34 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    func createLabels(){
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello worolold"
+        myLabel.fontSize = 20
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        myLabel.name = "mainLabel"
+        self.addChild(myLabel)
+    }
+    func createButtons(){
+        let b1 = SKSpriteNode(imageNamed: "myAssets/button1.png")
+        let b1S = getRelativeScale(scene!.size.width, itemThingy: b1.size.width, desiredRatio: 0.1)
+        b1.xScale = b1S
+        b1.yScale = b1S
+        b1.position = CGPoint(x:CGRectGetMaxX(self.frame) - b1.size.width, y:CGRectGetMidY(self.frame))
+        b1.name = "upButton1"
+        self.addChild(b1)
+        
+    }
+    func createCannon(){
+        
+    }
+    func createOwl(){
+        
+    }
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        myLabel.text = "Not a Jew. Rekt"
-        
-        self.addChild(myLabel)
+        createLabels()
+        createButtons()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -26,7 +45,13 @@ class GameScene: SKScene {
         for touch in touches {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            let b1 = self.childNodeWithName("upButton1") as! SKSpriteNode
+            if b1.containsPoint(location){
+                adjustAngle(5)
+                adjustPower(2)
+            }
+            
+            /*let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
             sprite.xScale = 0.5
             sprite.yScale = 0.5
@@ -36,11 +61,13 @@ class GameScene: SKScene {
             
             sprite.runAction(SKAction.repeatActionForever(action))
             
-            self.addChild(sprite)
+            self.addChild(sprite)*/
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        let l = self.childNodeWithName("mainLabel") as! SKLabelNode
+        l.text = "power: \(cannonPower) || angle: \(cannonAngle)"
     }
 }
---
+++ inTheOwl/logic.swift
@@ -0,0 +1,35 @@
+//
+//  logic.swift
+//  inTheOwl
+//
+//  Created by Brandon Wallace on 6/4/16.
+//  Copyright © 2016 Made On Planes. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+var cannonAngle: Int = 0
+var cannonPower: Int = 0
+
+let cannonMax = 90
+let cannonMin = 0
+
+let cannonPowerMax = 100
+let cannonPowerMin = 0
+
+func adjustAngle(by: Int){
+    if (cannonAngle + by <= cannonMax && cannonAngle + by >= cannonMin) {
+        cannonAngle += by
+    }
+}
+
+func adjustPower(by: Int){
+    if (cannonPower + by <= cannonPowerMax && cannonPower + by >= cannonPowerMin){
+        cannonPower += by
+    }
+}
+
+func getRelativeScale(screenThingy: CGFloat, itemThingy: CGFloat, desiredRatio: CGFloat)->CGFloat {
+    return desiredRatio * screenThingy / itemThingy
+}
