bef2ea708e59b0c63ea530cc106688b0b0d7d5f3
Brandon Wallace <branwall@me.com>
2016-06-05 21:35:30 -0700

    Making the firing dynamic; adding debug label.

--- inTheOwl/GameScene.swift
+++ inTheOwl/GameScene.swift
@@ -8,6 +8,9 @@
 
 import SpriteKit
 
+let bitOwl: UInt32 = 1
+let bitWall: UInt32 = 1 << 1
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
     let can1 = SKSpriteNode(imageNamed: "myAssets/cannon1.png")
     
@@ -20,6 +23,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(myLabel)
         
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody?.contactTestBitMask = bitOwl
+        self.physicsBody?.categoryBitMask = bitWall
         self.physicsWorld.contactDelegate = self
         
         
@@ -29,18 +34,45 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let b1S = getRelativeScale(scene!.size.width, itemThingy: b1.size.width, desiredRatio: 0.1)
         b1.xScale = b1S
         b1.yScale = b1S
-        b1.position = CGPoint(x:CGRectGetMaxX(self.frame) - b1.size.width, y:CGRectGetMidY(self.frame))
+        b1.position = CGPoint(x:CGRectGetMaxX(self.frame) - 5/2 * b1.size.width, y:CGRectGetMinY(self.frame) + 3 * b1.size.height / 2)
         b1.name = "upButton1"
         b1.physicsBody = nil
+        let b1L = SKLabelNode(text: "Adjust Power")
+        b1L.fontSize = 15
+        b1L.position = CGPoint(x: b1.position.x, y: b1.position.y + b1.size.height)
         
         let b2 = SKSpriteNode(imageNamed: "myAssets/button1.png")
         b2.xScale = b1.xScale
-        b2.yScale = b1.xScale
+        b2.yScale = b1.yScale * -1
         b2.position = CGPoint(x:b1.position.x, y:b1.position.y - b2.size.height)
         b2.name = "downButton1"
         
+        let b3 = SKSpriteNode(imageNamed: "myAssets/button1.png")
+        b3.xScale = b1S
+        b3.yScale = b1S
+        b3.position = CGPoint(x:b1.position.x + (3/2) * b1.size.width, y: b1.position.y)
+        b3.name = "upButton2"
+        b3.physicsBody = nil
+        let b3L = SKLabelNode(text: "Adjust Angle")
+        b3L.position = CGPoint(x: b3.position.x, y: b3.position.y + b3.size.height)
+        b3L.fontSize = 15
+        
+        let b4 = SKSpriteNode(imageNamed: "myAssets/button1.png")
+        b4.xScale = b1S
+        b4.yScale = b1S * -1
+        b4.position = CGPoint(x:b3.position.x, y:b3.position.y - b3.size.height)
+        b4.name = "downButton2"
+        
+        
+        b1L.fontColor = UIColor.blueColor()
+        b3L.fontColor = b1L.fontColor
+        
         self.addChild(b1)
         self.addChild(b2)
+        self.addChild(b1L)
+        self.addChild(b3)
+        self.addChild(b4)
+        self.addChild(b3L)
         
     }
     func createCannon(){
@@ -52,15 +84,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         can1.anchorPoint = CGPoint(x: 0.27, y: 0.2)
         self.addChild(can1)
     }
-    func createOwl(){
+    func createOwl() -> SKSpriteNode{
         let x = SKSpriteNode(imageNamed: "myAssets/cannon1.png")
-        x.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        x.position = can1.position //CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         let c1S = getRelativeScale(scene!.size.width, itemThingy: can1.size.width, desiredRatio: 0.1)
         x.xScale = c1S
         x.yScale = c1S
         x.physicsBody = SKPhysicsBody(texture: x.texture! , size: x.size)
         x.name = "owl"
+        x.physicsBody?.categoryBitMask = bitOwl
+        x.physicsBody?.contactTestBitMask = bitWall
         self.addChild(x)
+        return x
         
     }
     override func didMoveToView(view: SKView) {
@@ -68,7 +103,77 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createLabels()
         createButtons()
         createCannon()
-        createOwl()
+        //createOwl()
+    }
+    func blowUpOwl(s: SKSpriteNode){
+        //s . animate with textures
+        s.color = UIColor.redColor()
+        s.colorBlendFactor = 1
+        let a = SKAction.applyImpulse(CGVector.zero, atPoint: s.position, duration: 1)
+        s.runAction(a) {
+            s.removeAllActions()
+            s.removeFromParent()
+        }
+    }
+    
+    func noOwl() -> Bool {
+        let x = self.childNodeWithName("owl")
+        if x == nil {
+            return true
+        }
+        return false
+    }
+    func noCollisionLabel() -> Bool {
+        let x = self.childNodeWithName("collLabel")
+        if x == nil {
+            return true
+        }
+        return false
+    }
+    func makeCollideLabel(i: CGFloat){
+        let x = SKLabelNode(text: "Collision: \(i) N/s")
+        x.fontSize = 25
+        x.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMaxY(self.frame) - x.fontSize)
+        let a = SKAction.colorizeWithColor(UIColor.blueColor(), colorBlendFactor: 1, duration: 1)
+        self.addChild(x)
+        x.name = "collLabel"
+        x.runAction(a) {
+            x.removeAllActions()
+            x.removeFromParent()
+        }
+
+        
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        print("contact")
+        let b1 = contact.bodyA
+        let b2 = contact.bodyB
+        
+        
+        let b1s = b1.node as? SKSpriteNode
+        let b2s = b2.node as? SKSpriteNode
+        
+        if b1s != nil {
+            print("b1")
+            if b1s!.name == "owl" {
+                blowUpOwl(b1s!)
+                if noCollisionLabel(){
+                    makeCollideLabel(contact.collisionImpulse)
+                }
+            }
+            
+        } else if (b2s != nil) {
+            print("b2")
+            if b2s!.name == "owl" {
+                blowUpOwl(b2s!)
+                if noCollisionLabel(){
+                    makeCollideLabel(contact.collisionImpulse)
+                }
+            }
+            
+        }
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -79,15 +184,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             let b1 = self.childNodeWithName("upButton1") as! SKSpriteNode
             let b2 = self.childNodeWithName("downButton1") as! SKSpriteNode
+            let b3 = self.childNodeWithName("upButton2") as! SKSpriteNode
+            let b4 = self.childNodeWithName("downButton2") as! SKSpriteNode
+            
             if b1.containsPoint(location){
-                adjustAngle(5)
                 adjustPower(2)
             } else if b2.containsPoint(location) {
-                adjustAngle(-5)
                 adjustPower(-2)
-            } else {
-                let x = self.childNodeWithName("owl") as! SKSpriteNode
-                
+            } else if b3.containsPoint(location) {
+                adjustAngle(5)
+            } else if b4.containsPoint(location) {
+                adjustAngle(-5)
+            } else if can1.containsPoint(location) && noOwl() {
+                //let x = self.childNodeWithName("owl") as! SKSpriteNode
+                let x = createOwl()
                 x.physicsBody?.applyImpulse(force())
                 
             }
